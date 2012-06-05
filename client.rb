77ab5b30134a131c18cca5097587d3ec1f814873
Ezekiel Templin <zeke@templ.in>
2012-06-05 20:46:55 -0300

    Give that a try.

--- client.rb
+++ client.rb
@@ -4,8 +4,8 @@ require 'faraday_middleware'
 connection = Faraday.new('http://localhost:4567') do |builder|
   builder.response :follow_redirects
 
-  builder.request :multipart
   builder.request :url_encoded
+  builder.request :multipart
 
   builder.adapter :net_http
 end
